297b850ab7bd148e7707900e0bccb50d71978839
Jawad Alkassim <48631694+jawadalkassim@users.noreply.github.com>
2024-02-18 21:16:33 +0300

    few chanegs to the page

--- v4/index.php
+++ v4/index.php
@@ -17,7 +17,15 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;700&display=swap" rel="stylesheet">
 
+ <!-- Google tag (gtag.js) -->
+ <script async src="https://www.googletagmanager.com/gtag/js?id=AW-"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'AW-');
+</script>
  
     <script>
         var urlParams = new URLSearchParams(window.location.search);
