763fcfc87947cd835e40fb874ec7a3be3f503e05
Jawad Alkassim <48631694+jawadalkassim@users.noreply.github.com>
2024-02-24 02:15:35 +0300

    add jornaya

--- v4/index.php
+++ v4/index.php
@@ -415,7 +415,8 @@ window._loq =[]
                             </div>
                             <form id="msform" class="form" action="thank-you.php?" novalidate method="post">
                                 <input type="hidden" id="token" name="token" value="9741a0c84c1112244e3cce9df3fb31a17217692194c4cd5ffef12c033d5eb9f6">
-                                <!-- <input id="leadid_token" name="jornaya_lead_id" type="hidden" value="" /> -->
+                                <input id="leadid_token" name="universal_leadid" type="hidden" value=""/>
+
                                 <input type="hidden" id="tcpa_text" name="tcpa_text" value="By clicking Next, I agree to Terms, Privacy, and consent to solar/home servicers to send marketing prerecorded messages and autodialed calls/texts to my phone number above even if it's on any do not call list. Consent is not a condition of purchase. You can opt-out at any time (see Terms). Message/data rates may apply.">
                                 <input type="hidden" id="tcpa_consent" name="tcpa_consent" value="Yes">
                                 <input type="hidden" id="interested_in_solar_electric" name="interested_in_solar_electric" value="yes">
