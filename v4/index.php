3291e1e9b9746c4c07378dd0a54e2ae37e197bf5
Jawad Alkassim <48631694+jawadalkassim@users.noreply.github.com>
2024-02-24 22:12:32 +0300

    add gtag

--- v4/index.php
+++ v4/index.php
@@ -38,10 +38,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-KWW3RGHZ');</script>
 <!-- End Google Tag Manager -->
 
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KWW3RGHZ"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
 
 
 
