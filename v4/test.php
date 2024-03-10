d20cfb5099c798d041c5b594d2a57319b287e895
Jawad Alkassim <48631694+jawadalkassim@users.noreply.github.com>
2024-03-10 05:29:37 +0300

    add sending leads to ghl

--- v4/test.php
+++ v4/test.php
@@ -17337,7 +17337,7 @@
                         firstName: first_name || "uknown",
                         lastName: last_name || "unknown",
                         email: email || "unknown",
-                        phone: phone_home || 25234567890,
+                        phone: phone_home || 2523456789,
                         address1: address || "uknown",
                         city: city || "uknown",
                         state: state || "uknown",
