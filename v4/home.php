8acfc98586bb2b1d0a6f99374db9aa7c609fc358
Jawad Alkassim <48631694+jawadalkassim@users.noreply.github.com>
2024-04-14 23:35:09 +0300

    open offer in the same tab

--- v4/home.php
+++ v4/home.php
@@ -55,7 +55,7 @@
                 <div class="navbar-collapse collapse  w-100" id="navigation">
                     <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
                         <li class="nav-item">
-                            <a class="nav-link btn btn-grad text-uppercase" href="https://trk.truesolarrates.com/click" target="_blank">
+                            <a class="nav-link btn btn-grad text-uppercase" href="https://trk.truesolarrates.com/click" >
                                 See If You Qualify <i></i>
                                 <span class="btn-bg grad"></span>
                             </a>
@@ -129,7 +129,7 @@
                         <p>
                             Since every state offers different incentives, we found that using the 
                             <b>
-                                <a href="https://trk.truesolarrates.com/click" target="_blank">free Clever Solar Techs™ Solar Tool</a>
+                                <a href="https://trk.truesolarrates.com/click" >free Clever Solar Techs™ Solar Tool</a>
                             </b>
                             is the 
 				best way to find out which solar incentives are offered in your area. 
@@ -141,7 +141,7 @@
                         <p>
                             <b>Step 1:</b>
                             <b>
-                                <a href="https://trk.truesolarrates.com/click" target="_blank">Click here or tap your state below to get started.</a>
+                                <a href="https://trk.truesolarrates.com/click" >Click here or tap your state below to get started.</a>
                             </b>
                         </p>
                         <p>
@@ -149,7 +149,7 @@
                             Once you enter your zip code and enter some basic home info, you'll be able to see if you qualify! It's really that easy!
                         </p>
                         <div class="articleMap">
-                            <a href="https://trk.truesolarrates.com/click" onClick="return true;" target="_blank">
+                            <a href="https://trk.truesolarrates.com/click" onClick="return true;" >
                                 <svg version="1.1" xmlns="https://www.w3.org/2000/svg" xmlns:xlink="https://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 600 340" style="enable-background:new 0 0 600 340;" xml:space="preserve">
                                     <g id="Wyoming">
                                         <path class="stateImage" d="M199.07,80.68c-12.9-0.98-28.28-2.68-44.46-4.89c-4.27-0.59-10.06-1.34-12.74-1.6
